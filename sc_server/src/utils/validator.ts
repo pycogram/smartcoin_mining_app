121aa8b6adbd13759e64fb43464f0bed19ef083c
pycogram <kaluifesinachi@yahoo.com>
2025-06-27 21:24:03 +0100

    to validate and sanitize user input

---
+++ sc_server/src/utils/validator.ts
@@ -0,0 +1,49 @@
+import Joi from "joi";
+
+// Reusable schema for name fields
+const nameField = Joi.string()
+    .required()
+    .min(2)
+    .max(30)
+    .messages({
+        'string.base': '{{#label}} must be a string',
+        'string.empty': '{{#label}} is required',
+        'string.min': '{{#label}} must be at least 2 characters',
+        'string.max': '{{#label}} must not exceed 30 characters',
+        'any.required': '{{#label}} is required'
+});
+const emailField = Joi.string()
+    .email({tlds: {allow: ['com', 'net']}})
+    .required()
+    .min(6)
+    .max(60)
+    .messages({
+        'string.base': '{{#label}} must be a string',
+        'string.email': '{{#label}} must be a valid and end with either .com or .net',
+        'string.empty': '{{#label}} is required',
+        'string.min': '{{#label}} must be at least 6 characters',
+        'string.max': '{{#label}} must not exceed 60 characters',
+        'any.required': '{{#label}} is required'
+});
+const passwordField = Joi.string()
+    .required()
+    .min(5)
+    .max(30)
+    .pattern(new RegExp('^[a-zA-Z0-9@#$!]{3,30}$'))
+    .messages({
+        'string.base': '{{#label}} must be a string',
+        'string.empty': '{{#label}} is required',
+        'string.min': '{{#label}} must be at least 6 characters',
+        'string.max': '{{#label}} must not exceed 30 characters',
+        'string.pattern.base': '{{#label}} must be alphanumeric and can include @ # $ !',
+        'any.required': '{{#label}} is required'
+    });
+
+const registerUserSchema = Joi.object({
+    first_name: nameField.label("First name"),
+    last_name: nameField.label("Last name"),
+    email: emailField.label("Email"),              
+    password: passwordField.label("Password")
+});
+
+export {registerUserSchema};
