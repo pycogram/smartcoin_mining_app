b4bf3f5ba768cafbc35a791508dcd09cf7794d24
pycogram <kaluifesinachi@yahoo.com>
2025-06-27 21:09:25 +0100

    updated file with correct db connection url

--- sc_server/src/server.ts
+++ sc_server/src/server.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import path from 'path';
 import dotenv from 'dotenv';
 import { userRoutes } from './routes/user';
@@ -7,12 +8,30 @@ dotenv.config({ path: path.resolve(__dirname, '../../.env') });
 
 const app = express(); //instanciate an express obj to server with it
 
-// endpoint and route handlers
+// middleware that allow json
+app.use(express.json());
 
-app.use('/api/user', userRoutes)
+// middleware that allow url encoded data
+app.use(express.urlencoded({ extended: true }));
+
+// endpoint and route handlers
+app.use('/api/user', userRoutes);
 
 app.get('/', (req, res) => {
     res.send('welcome to smartcoin');               
 });
 
-app.listen(process.env.PORT || 5000, () => console.log("listening to the server"));
+const {DB_URL, PORT} = process.env;
+
+if (!DB_URL || !PORT) {
+  throw new Error("Missing DB_URL in environment variables");
+}
+// connect to db
+mongoose.connect(DB_URL).then(() => {
+
+    console.log(`connected to DB successfully`);
+    app.listen(PORT || 5000, () => console.log(`listening to the server`));
+
+}).catch((err) => {
+    console.log(`DB error - ${err}`)
+})
