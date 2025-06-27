02ec6a222c64a34ed2237204a9c2f26e765d00c9
pycogram <kaluifesinachi@yahoo.com>
2025-06-27 21:20:11 +0100

    uses mongodb (NoSql) to user data in json format

---
+++ sc_server/src/models/user.ts
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const userScheme = new mongoose.Schema({
+    first_name: {
+        type: String,
+        require: [true, "first name is required"],
+        trim: true    
+    },
+    last_name: {
+        type: String,
+        require: [true, "last name is required"],
+        trim: true
+    },
+    email: {
+        type: String,
+        require: [true, 'email address is required'],
+        lowercase: true,
+        trim: true,
+        unique: true
+    }, 
+    invited_by: {
+        type: String,
+        trim: true
+    },
+    password: {
+        type: String,
+        require: [true, 'password is required'],
+        select: false,
+        trim: true
+    }
+}, { timestamps: true });
+
+const userModel = mongoose.model('users', userScheme);
+export default userModel;
