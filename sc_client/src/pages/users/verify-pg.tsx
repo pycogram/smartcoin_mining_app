ca45b039e5636e9aa516bf38c3652a96197f5592
pycogram <kaluifesinachi@yahoo.com>
2025-06-29 17:01:26 +0100

    added state property of location that receive items setup on the register page

--- sc_client/src/pages/users/verify-pg.tsx
+++ sc_client/src/pages/users/verify-pg.tsx
@@ -1,15 +1,25 @@
 import "../../css/page_css/user_css/verify-pg.css";
 import SecondaryBtn from "../../components/button/secondary-btn";
 import verify_pic from "../../images/pics/verify-illustraction.png";
+import { useLocation } from "react-router-dom";
 
 const VerifyPage = () => {
+    // get items stored in state passed from registration page
+    const location = useLocation();
+    console.log(location.state ?? "");
+    const userInfo =  location?.state ?? ""
+
+    //input state
+    const first_name = userInfo?.first_name;
+    const email = userInfo?.email;
+
     return ( 
         <div className="verify-page">
             <form> 
                 <header className="header-text">Verify</header>
                 <p className="verify-info">
-                    Hi, Daniel. Your account has been registered succesfully. 
-                    In order to make use of your account ( <b><i> daniel@gmail.com </i></b> ). 
+                    Hi, <b>{first_name}</b>. Your account has been registered succesfully. 
+                    In order to make use of your account (<b><i> {email} </i></b>). 
                     Please hit the verify button to receive email verification code.
                 </p>
                 <img src={verify_pic} alt="" />
