8f8c89dd832dbb78e12fb496f0427f8c3a6bd779
pycogram <kaluifesinachi@yahoo.com>
2025-06-29 16:58:27 +0100

    this get user input from the register page before sending it to the backend or server for processing

--- sc_server/src/controllers/user.ts
+++ sc_server/src/controllers/user.ts
@@ -5,14 +5,17 @@ import { registerUserSchema } from "../utils/validator";
 import bcrypt from "bcrypt";
 
 const registerUser = async (req: Request, res: Response):Promise<void> => {
-    const {first_name, last_name, email, password, confirm_password} = req.body;
+    const {first_name, last_name, email, password, confirmed_password} = req.body;
+
+    //check if all input are empty
+    if(! first_name || ! last_name || ! email || ! password || ! confirmed_password) return errHandler(res, "all fields are required")
 
     // validate and sanitize user inputs
-    const {error, value} = registerUserSchema.validate({first_name, last_name, email, password})
-    if(error) return errHandler(res, error.details[0].message);
+    const {error, value} = registerUserSchema.validate({first_name, last_name, email, password});
+    if(error) return errHandler(res, error.details[0].message.replace(/"/g, ""));
 
     // check if password and confirm password are the same
-    if(password !== confirm_password) return errHandler(res, "confirm password does not match");
+    if(password !== confirmed_password) return errHandler(res, "confirm password does not match");
 
     //check if email exists
     const emailExist = await userModel.findOne({email});
@@ -28,11 +31,8 @@ const registerUser = async (req: Request, res: Response):Promise<void> => {
         res.status(200).json({
             status: `success`,
             message: `Account created successfully, To login, please request for email verification code.`,          
-            data: {
-                first_name: userRegistered.first_name,
-                last_name: userRegistered.last_name,
-                email: userRegistered.email,
-            }
+            first_name: userRegistered.first_name,
+            email: userRegistered.email,
         });
     } catch(err){
         //console.log("Handled error:", (err as Error).message);
