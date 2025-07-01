15dfe130ebe6ac75b4931e5433db8caf69919c0a
pycogram <kaluifesinachi@yahoo.com>
2025-07-01 05:21:49 +0100

    added verify user route

--- sc_server/src/routes/user.ts
+++ sc_server/src/routes/user.ts
@@ -1,9 +1,10 @@
 import express from 'express';
-import { registerUser } from '../controllers/user';
+import { registerUser, verifyUser } from '../controllers/user';
 
 const router = express.Router();
 
 // create a user routes
 router.post('/register', registerUser);
+router.post('/verify', verifyUser);
 
 export { router as userRoutes };
