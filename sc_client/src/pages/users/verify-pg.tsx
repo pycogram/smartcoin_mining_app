e211c348749ff86a4a2d3496e1cdb3275891723d
pycogram <kaluifesinachi@yahoo.com>
2025-07-01 05:16:26 +0100

    added route to the client verify page so it can communicate with the backend by requesting the email verification code

--- sc_client/src/pages/users/verify-pg.tsx
+++ sc_client/src/pages/users/verify-pg.tsx
@@ -1,7 +1,10 @@
 import "../../css/page_css/user_css/verify-pg.css";
 import SecondaryBtn from "../../components/button/secondary-btn";
 import verify_pic from "../../images/pics/verify-illustraction.png";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
+import Fail from "../../components/alert/fail";
+import { useState } from "react";
+import { verifyUser } from "../../controllers/user";
 
 const VerifyPage = () => {
     // get items stored in state passed from registration page
@@ -13,10 +16,30 @@ const VerifyPage = () => {
     const first_name = userInfo?.first_name;
     const email = userInfo?.email;
 
+    //error state
+    const [error, setError] = useState<string>("");    
+    
+    const navigate = useNavigate();
+    
+    const handleVerify = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        // register user
+        try{
+            const data = await verifyUser(email);
+
+            const {message} = data;
+            navigate('/confirm', {state: {message}});
+
+        } catch(err){
+            setError(`${(err as Error).message}`)
+        }
+    }
+
     return ( 
         <div className="verify-page">
-            <form> 
+            <form onSubmit={handleVerify}> 
                 <header className="header-text">Verify</header>
+                {error && <Fail error={error} />}
                 <p className="verify-info">
                     Hi, <b>{first_name}</b>. Your account has been registered succesfully. 
                     In order to make use of your account (<b><i> {email} </i></b>). 
