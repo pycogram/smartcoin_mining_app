f423d2715cf699654c8b1247a8dda6a062f9a414
pycogram <kaluifesinachi@yahoo.com>
2025-07-05 00:41:00 +0100

    implemted a route protector, all users must have a valid jwt token before accessing some certain page

---
+++ sc_server/src/middlewares/auth-user.ts
@@ -0,0 +1,34 @@
+import { Request, Response, NextFunction} from "express"; 
+import { errHandler } from "../utils/error-handler";
+import { verifyUserToken } from "../token/tokenized-user";
+import userModel from "../models/user";
+
+export const authUser = async(req: Request, res:Response , next:NextFunction): Promise<void> => {
+    // grab the token from either req header or cookie based on the environment
+    let token = req.headers.client === 'not-browser' 
+                ? req.headers.authorization 
+                : req.cookies['Authorization'];
+
+    if (!token) {
+        return errHandler(res, "Unauthorized access, please login");
+    }
+
+    let originalToken =  token.split(" ")[1];
+
+    const verifiedToken = verifyUserToken(originalToken);
+
+    if (typeof verifiedToken === "string") {
+        const errorMessage = verifiedToken.includes("expired")
+            ? "Session expired, please log in again"
+            : "Invalid token, please log in again";
+        
+        return errHandler(res, errorMessage);
+    }
+    
+    // save the user in request
+    let { user_id } = verifiedToken;
+
+    (req as any).user_id = user_id;
+    
+    next();
+}
