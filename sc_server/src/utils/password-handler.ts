a16325f4c32fe07bfc2017028896491a3add13fd
pycogram <kaluifesinachi@yahoo.com>
2025-07-04 04:09:05 +0100

    implemented verify password feature after hash password was done a few hrs ago

--- sc_server/src/utils/password-handler.ts
+++ sc_server/src/utils/password-handler.ts
@@ -1,8 +1,12 @@
 import bcrypt from "bcrypt";
 
-const PasswordHash = async (password : string): Promise<string> => {
+const PasswordHash = async (password: string): Promise<string> => {
     const saltNum = await bcrypt.genSalt(10);
     return bcrypt.hash("" + password, saltNum);
 }
 
-export {PasswordHash}
+const PasswordVerify = async(password: string, hashedPassword: string): Promise<boolean> =>{
+    return bcrypt.compare(password, hashedPassword)
+}
+
+export {PasswordHash, PasswordVerify}
