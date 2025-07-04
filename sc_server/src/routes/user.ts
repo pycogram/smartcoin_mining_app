0f944d744f7a8da797fb1c1c78274dd49ea20d70
pycogram <kaluifesinachi@yahoo.com>
2025-07-04 04:13:51 +0100

    add routes for confirm user and login user

--- sc_server/src/routes/user.ts
+++ sc_server/src/routes/user.ts
@@ -1,10 +1,12 @@
 import express from 'express';
-import { registerUser, verifyUser } from '../controllers/user';
+import { confirmUser, loginUser, registerUser, verifyUser } from '../controllers/user';
 
 const router = express.Router();
 
 // create a user routes
 router.post('/register', registerUser);
 router.post('/verify', verifyUser);
+router.post('/confirm', confirmUser);
+router.post('/login', loginUser);
 
 export { router as userRoutes };
