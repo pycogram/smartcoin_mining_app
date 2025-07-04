9bcad37d7e4bbf177fc9ce164f9cd3fab08f4573
pycogram <kaluifesinachi@yahoo.com>
2025-07-05 00:47:05 +0100

    added verify and login route for users to make a request with it

--- sc_client/src/controllers/user.ts
+++ sc_client/src/controllers/user.ts
@@ -1,6 +1,5 @@
-//register user
-
-const registerUser = async (formData : object) => {
+// register user
+const registerUser = async (formData: object) => {
 
     const res = await fetch('/api/user/register', {
         method: 'POST', 
@@ -18,9 +17,8 @@ const registerUser = async (formData : object) => {
 
     return data;
 }
-
 // verify user
-const verifyUser = async (email : string) => {
+const verifyUser = async (email: string) => {
     const res = await fetch("api/user/verify", {
         method: 'POST', 
         headers: {
@@ -36,5 +34,41 @@ const verifyUser = async (email : string) => {
 
     return data;
 }
+// confirm user
+const confirmUser = async (code: string, id: string) => {
+    const res = await fetch("api/user/confirm", {
+        method: 'POST', 
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({code, id})
+    });
+    const data = await res.json();
 
-export { registerUser , verifyUser};
+    if(! res.ok){
+        throw new Error(`${data.message}`);
+    }
+
+    return data;
+}
+// login user
+const loginUser = async (formData: object) => {
+
+    const res = await fetch('/api/user/login', {
+        method: 'POST', 
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({...formData})
+    });
+
+    const data = await res.json();
+
+    if(! res.ok){
+        throw new Error(`${data.message}`);
+    }
+
+    return data;
+}
+
+export { registerUser , verifyUser, confirmUser, loginUser};  
