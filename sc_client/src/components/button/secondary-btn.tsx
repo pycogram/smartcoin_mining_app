1932944e13e42b980bb8b53b668cbf49e2c86648
pycogram <kaluifesinachi@yahoo.com>
2025-07-05 00:51:04 +0100

    added active property to be activate when user press and hold the button

--- sc_client/src/components/button/secondary-btn.tsx
+++ sc_client/src/components/button/secondary-btn.tsx
@@ -1,23 +1,27 @@
 import { JSX } from "react";
 import "../../css/component_css/button/secondary-btn.css";
+import { Link} from "react-router-dom";
 
 type SecondaryBtnProp = {
-    text_1 : string,
-    text_2 : string,
-    icon_1 ?: JSX.Element,
-    icon_2 ?: JSX.Element
+    text_1: string,
+    text_2: string,
+    icon_1?: JSX.Element,
+    icon_2?: JSX.Element,
+    link_1?: any,
 }
 
-const SecondaryBtn = ({text_1, text_2, icon_1, icon_2}: SecondaryBtnProp) => {
+const SecondaryBtn = ({text_1, text_2, icon_1, icon_2, link_1}: SecondaryBtnProp) => {
     return (
         <span className="secondary-btn">
-            <button className="s-btn1">
-                {icon_1 || <i className="fa-solid fa-arrow-left icon-btn"></i>}
-                {text_1}
-            </button>
+            <Link to={link_1}>
+                <button className="s-btn1">
+                    {icon_1 || <i className="fa-solid fa-arrow-left icon-btn"></i>}
+                    {text_1}
+                </button>
+            </Link>
             <button className="s-btn1 s-btn2">
-                {text_2}
-                {icon_2 || <i className="fa-solid fa-arrow-right icon-btn"></i>}
+                    {text_2}
+                    {icon_2 || <i className="fa-solid fa-arrow-right icon-btn"></i>}
             </button>
         </span>
      );
