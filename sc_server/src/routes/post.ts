a2d197c36e9d3703cbc2989be23b0006e50fa403
pycogram <kaluifesinachi@yahoo.com>
2025-07-09 02:28:02 +0100

    added endpoint routes for post and user

---
+++ sc_server/src/routes/post.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import { createPost, deletePost, updatePost, viewAllPost, viewPost } from '../controllers/post';
+import { authUser } from '../middlewares/auth-user';
+
+const router = express.Router();
+
+// create post routes
+
+router.post('/create-post', authUser, createPost);
+router.delete('/delete/:post_id', authUser, deletePost);
+router.put('/update/:post_id', authUser, updatePost);
+router.get('/view-post', authUser, viewPost);
+router.get('/feed', authUser, viewAllPost);
+
+export { router as postRoutes };
--- sc_server/src/routes/user.ts
+++ sc_server/src/routes/user.ts
@@ -1,9 +1,9 @@
 import express from 'express';
 import { 
-    confirmUser, dashboardUser, loginUser, registerUser, 
+    confirmUser, loginUser, registerUser, 
     verifyUser 
 } from '../controllers/user';
-import { authUser } from '../middlewares/auth-user';
+//import { authUser } from '../middlewares/auth-user';
 
 const router = express.Router();
 
@@ -12,6 +12,6 @@ router.post('/register', registerUser);
 router.post('/verify', verifyUser);
 router.post('/confirm', confirmUser);
 router.post('/login', loginUser);
-router.post('/dashboard', authUser, dashboardUser);
+//router.post('/dashboard', authUser, dashboardUser);
 
 export { router as userRoutes };
