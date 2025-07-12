872fc7bdef18f0ee47d8f1959ab79b0790efe415
pycogram <kaluifesinachi@yahoo.com>
2025-07-13 00:37:18 +0100

    this alert based frontend feature that tells uers when something good happen. like- successful reg and login

---
+++ sc_client/src/components/alert/success.tsx
@@ -0,0 +1,24 @@
+import "../../css/component_css/alert/success.css";
+
+type SuccessProp = {
+    success: string;
+    loggedinStatus?: boolean;
+}
+
+const Success = ({success, loggedinStatus = false} : SuccessProp) => {
+    const message = success;
+    const saIN = loggedinStatus && "sa-in";
+    return ( 
+        <div className={ `success-alert ${saIN}`}> 
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+            </svg>
+            <p>{message}</p>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
+            <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 12.75 6 6 9-13.5" />
+            </svg>
+        </div>
+     );
+}
+ 
+export default Success;
