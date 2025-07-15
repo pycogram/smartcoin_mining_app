fb685553b6288ff0aa3fd6fd8e476a76aeb0b815
pycogram <kaluifesinachi@yahoo.com>
2025-07-16 00:05:06 +0100

    implemented a logic counter that keep mining live based on the ending time and current time the db recorded and returned.

--- sc_client/src/pages/users/dashboard.tsx
+++ sc_client/src/pages/users/dashboard.tsx
@@ -6,22 +6,135 @@ import gift from "../../images/pics/gift.png";
 import { Link} from "react-router-dom";
 import pdp from "../../images/pics/pdp.png";
 import Piechart from "../../components/chart/piechart";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import Success from "../../components/alert/success";
-
+import { UserContext } from "../../contexts/user";
+import { mineDetail, mineSc } from "../../controllers/dashboard";
+import Fail from "../../components/alert/fail";
 
 const Dashboard = () => {
     const message_user = localStorage.getItem("message_user") ?? "";
 
     const [perfect, setPerfect] = useState<string>(message_user);
+    const {user} = useContext(UserContext)!;
+    const [hideBal, setHideBal] = useState<boolean>(false);
+    const [minedSc, setMinedSc] = useState<number>(0);
+    const [minedScStatus, setMinedScStatus] = useState<boolean>(false);
+    const [monitorMine, setMonitorMine] = useState<boolean>(false);
+    const [endTime, setEndTime] = useState<number>(0);
+    const [totalSc, setTotalSc] = useState<number>(0);
+    
+    useEffect(() => {
+        const interval = setInterval(() => {
+            const currentTime = Date.now(); 
+            if (endTime && currentTime <= endTime) {
+                setMinedSc((prev) => {
+                    if(prev >= totalSc){
+                        clearInterval(interval);
+                        return prev;
+                    }
+                    return prev + 1;
+                });
+            } else {
+                clearInterval(interval);
+            }
+        }, 4000);
+
+        return () => clearInterval(interval);
+    }, [monitorMine]);
+
+    window.addEventListener("beforeunload", () => {
+        setMinedSc(0);
+    })
 
     useEffect(()=> {
+
         setTimeout(()=> {
             setPerfect("");
             localStorage.removeItem("message_user");
         }, 5 * 1000);
+
+        setTimeout(async ()=> {
+            try{
+                const {data} = await mineDetail();
+
+                const endTime = new Date(data.end_time).getTime();
+                const total_mined = data.total_mined;
+                const nowTime = Date.now();
+                const diffTime =  Math.floor((nowTime - endTime) / 1000);
+                const scPerTime = Math.abs((450 / 1800000) * diffTime);
+                const actualSc = Math.floor((data.total_mined - scPerTime));
+
+                if(nowTime < endTime){
+                    setMinedSc(actualSc + 1);
+                } else {
+                    setMinedSc(total_mined);
+                }
+                
+                setTotalSc(total_mined);
+                setEndTime(endTime);
+                setMonitorMine(! monitorMine);
+                setMinedScStatus(! minedScStatus);
+                    
+            } catch(err){
+                let message =  (err as Error).message;
+                setError(message);
+
+            } finally{
+                setTimeout(() => {
+                    setPerfect("");
+                    setError("");
+                }, 10 * 1000);
+            }
+        }, 0);
+      
     }, []);
 
+    // hide and reveal balance
+    const hideRevealBalance = () => {
+        setHideBal(! hideBal);
+        if(hideBal){
+            localStorage.setItem("hide-bal", "true");
+        } else {
+            localStorage.removeItem("hide-bal");
+        }
+    }
+    const hideBalStatus = localStorage.getItem("hide-bal");
+
+    // set error
+    const [error, setError] = useState<string>("");
+
+    const mineSCFn = async () => {
+        try{
+            setPerfect("");
+            setError("");
+
+            const {data, message} = await mineSc(true);
+
+            const endTime = new Date(data.end_time).getTime(); 
+                      
+            setEndTime(endTime);
+            setMinedSc(minedSc && minedSc + 1);
+            setMonitorMine(! monitorMine);
+            setPerfect(message);
+
+        } catch(err){
+            let message = (err as Error).message;
+            setError(message);
+
+        } finally {
+            setTimeout(() => {
+                setPerfect("");
+                setError("");
+            }, 10 * 1000);
+        }
+    }
+
+    // if(! minedSc ) return (
+    //     <div>
+    //         <i className="fa-solid fa-spinner lazy-page-load-icon"></i>
+    //     </div>
+    // )
     return ( 
         <div className="dashboard">
             <nav className="db-navbar">
@@ -42,34 +155,34 @@ const Dashboard = () => {
                         <span className="">
                             <img src={pdp} alt="profile pic" />
                         </span>
-                        <p className="">Ifesinachi .D</p>
+                        <p className="">{user?.first_name} {user?.last_name}</p>
                     </div>
                 </Link>
             </nav>
             <div className="logo-amt-eye">
                 {perfect && <Success success={`${perfect}`} loggedinStatus={true} />}
-                <img src={smartcoin_logo} alt="smartlogo" />
-                <span className="mined-amt">
-                    <h2>10,000</h2>
-                    <h3>SC</h3>
-                </span>
+                {error && <Fail error={error} loggedinStatus={true} />}
+                { !hideBalStatus ? <img src={smartcoin_logo} alt="smartlogo" /> : ""}
+                   <span className="mined-amt">
+                         { 
+                           !minedScStatus ? <h3>loading...</h3> :
+                                <h2>
+                                    {
+                                        !hideBalStatus  ? minedSc && minedSc < 100 
+                                                        ? minedSc + ".000" 
+                                                        : minedSc 
+                                                        : "*****"
+                                    } 
+                                </h2>
+                        }
+                        {  !minedScStatus ? "" : <h3>SC</h3> } 
+                    </span>
                 <span className="iconx">
-                    <i className="fa-solid fa-eye"></i>
+                    <i onClick={hideRevealBalance} className={!hideBalStatus ? "fa-solid fa-eye" : "fa-solid fa-eye-slash" }></i>
                 </span>
-
-                    {/* <i className="fa-solid fa-eye icon"></i> */}
-                    {/* <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512" className="iconx">
-                        <path d="M38.8 5.1C28.4-3.1 13.3-1.2 5.1 9.2S-1.2 34.7 9.2 42.9l592 464c10.4 8.2 25.5 6.3 33.7-4.1s6.3-25.5-4.1-33.7L525.6 386.7c39.6-40.6 66.4-86.1 79.9-118.4c3.3-7.9 3.3-16.7 0-24.6c-14.9-35.7-46.2-87.7-93-131.1C465.5 68.8 400.8 32 320 32c-68.2 0-125 26.3-169.3 60.8L38.8 5.1zM223.1 149.5C248.6 126.2 282.7 112 320 112c79.5 0 144 64.5 144 144c0 24.9-6.3 48.3-17.4 68.7L408 294.5c8.4-19.3 10.6-41.4 4.8-63.3c-11.1-41.5-47.8-69.4-88.6-71.1c-5.8-.2-9.2 6.1-7.4 11.7c2.1 6.4 3.3 13.2 3.3 20.3c0 10.2-2.4 19.8-6.6 28.3l-90.3-70.8zM373 389.9c-16.4 6.5-34.3 10.1-53 10.1c-79.5 0-144-64.5-144-144c0-6.9 .5-13.6 1.4-20.2L83.1 161.5C60.3 191.2 44 220.8 34.5 243.7c-3.3 7.9-3.3 16.7 0 24.6c14.9 35.7 46.2 87.7 93 131.1C174.5 443.2 239.2 480 320 480c47.8 0 89.9-12.9 126.2-32.5L373 389.9z"/>
-                    </svg> */}
-                    {/* <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" className="iconx">
-                        <path d="M288 32c-80.8 0-145.5 36.8-192.6 80.6C48.6 156 17.3 208 2.5 243.7c-3.3 7.9-3.3 16.7 0 24.6C17.3 304 48.6 356 95.4 399.4C142.5 443.2 207.2 480 288 480s145.5-36.8 192.6-80.6c46.8-43.5 78.1-95.4 93-131.1c3.3-7.9 3.3-16.7 0-24.6c-14.9-35.7-46.2-87.7-93-131.1C433.5 68.8 368.8 32 288 32zM144 256a144 144 0 1 1 288 0 144 144 0 1 1 -288 0zm144-64c0 35.3-28.7 64-64 64c-7.1 0-13.9-1.2-20.3-3.3c-5.5-1.8-11.9 1.6-11.7 7.4c.3 6.9 1.3 13.8 3.2 20.7c13.7 51.2 66.4 81.6 117.6 67.9s81.6-66.4 67.9-117.6c-11.1-41.5-47.8-69.4-88.6-71.1c-5.8-.2-9.2 6.1-7.4 11.7c2.1 6.4 3.3 13.2 3.3 20.3z"/>
-                    </svg> */}
-
-
-                {/* <i class="fa-solid fa-eye-slash"></i> */}
             </div>
             <div className="lock-coin">
-                <p>Locked: $SC 6,000</p>
+                <p>Locked: {0} SC</p>
             </div>
             <div className="db-button">
                 <span>
@@ -105,7 +218,7 @@ const Dashboard = () => {
                     </ul>
                 </div>
                 <div className="db-sidebutton">
-                    <span>
+                    <span onClick={mineSCFn}>
                         <img src={mine} alt="mining logo" />
                     </span>
                     <span>
