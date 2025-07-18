94c1ba20894b502df8f12c1e6193a08bb06ab11b
pycogram <kaluifesinachi@yahoo.com>
2025-07-18 22:33:19 +0100

    implemented a username generator logic by randomly generrating 4 characters plus the first name from the user

--- sc_server/src/token/generate-code.ts
+++ sc_server/src/token/generate-code.ts
@@ -4,5 +4,14 @@ export const generateCode = (length : number) => {
     for (let i = 0; i < length; i++) {
         result += chars.charAt(Math.floor(Math.random() * chars.length));
     }
+    return result.toString();
+}    
+
+export const generateUsername = (first_name: string, length : number) => {
+    const chars = '0123456789';
+    let result = first_name;
+    for (let i = 0; i < length; i++) {
+        result += chars.charAt(Math.floor(Math.random() * chars.length));
+    }
     return result;
 }    
