cc9cf4ceef9a754f13e9b040edb0407b2c646717
pycogram <kaluifesinachi@yahoo.com>
2025-07-24 01:54:59 +0100

    adjusted ref btn

--- sc_client/src/pages/users/update-profile.tsx
+++ sc_client/src/pages/users/update-profile.tsx
@@ -67,13 +67,14 @@ const UpdateProfile = () => {
             {error && <Fail error={error} loggedinStatus={true} />}
             {perfect && <Success success={`${perfect}`} loggedinStatus={true} />}
             <form onSubmit={handleUpdate} className="update-profile">
-                <span>
+                <span className="ref-span-go-back">
                     <Link to={'/dashboard'}>
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="m11.25 9-3 3m0 0 3 3m-3-3h7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                            <path strokeLinecap="round" strokeLinejoin="round" d="m11.25 9-3 3m0 0 3 3m-3-3h7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
                         </svg>
                     </Link>
                 </span>
+                
                 <div className="form-input-box login-form2">
                     <div className="update-img">
                         <img  src={pdp} alt="profile pic" />                  
