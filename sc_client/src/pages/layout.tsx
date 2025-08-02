00bf51ac426c2337ad312ea14529865dbdb2438c
pycogram <kaluifesinachi@yahoo.com>
2025-08-02 23:09:55 +0100

    added dvw to the body

--- sc_client/src/pages/layout.tsx
+++ sc_client/src/pages/layout.tsx
@@ -86,11 +86,11 @@ const Layout = () => {
                             <path strokeLinecap="round" strokeLinejoin="round" d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0" />
                         </svg>
                     </Link>
-                    {/* <div onClick={bgMode}>
+                    <div onClick={bgMode}>
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="nav-option">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" />
                         </svg>
-                    </div> */}
+                    </div>
                     <div>
                         <DarkModeToggle />
                     </div>
@@ -98,7 +98,7 @@ const Layout = () => {
                 <Link to={"/update-profile"}>
                     <div className="img-pdp">
                         <img src={pdp} alt="profile pic" />
-                        <p className="">{user.first_name} {user.last_name && '.' + user.last_name.charAt(0)}</p>
+                        <p className="">{user.first_name} {user.last_name && '.' + user.last_name.charAt(0).toUpperCase()}</p>
                     </div>
                 </Link>
             </nav>
