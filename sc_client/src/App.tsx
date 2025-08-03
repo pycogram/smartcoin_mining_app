3862b0560232fadc4e25b78ece3c248b6a141d49
pycogram <kaluifesinachi@yahoo.com>
2025-08-03 15:09:03 +0100

    added dark mode logic and colors

--- sc_client/src/App.tsx
+++ sc_client/src/App.tsx
@@ -1,5 +1,4 @@
 import React, { Suspense} from 'react';
-import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import UserRoutes from './routes/user';
 import NonUserRoutes from './routes/non-user';
